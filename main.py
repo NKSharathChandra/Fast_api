1d163318d41463bea5eea47b89c762fe7282e1df
NKSharathChandra <nksharathchandra4@gmail.com>
2023-04-21 17:08:33 +0530

    added the blog folder

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app=FastAPI()
 
@@ -7,7 +10,7 @@ def index():
     return { 'data':'blog list'} 
 
 @app.get('/blog')
-def index(limit=10,published:bool=True):
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
     if published:
         return {'data':f'{limit} blogs from the db'}
     else:
@@ -25,11 +28,24 @@ def show(id:int):
 @app.get('/blog/{id}/comments')
 def comments(id:int):
     if(id==1):
-      return {'data':{'comment1','comment2'}}
+      return {'data':{'comment1','comment2' }}
     elif(id==2):
        return {'data':{'comment3','comment4'}}
     else:
         return {'data':'other comments'}
+    
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {f"Thsi is the blog created with title {blog.title}"}
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=8000)
     
 
 
